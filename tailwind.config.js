9b44529309c28a10645fa29fe015b5f0cb41a53e
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-10-20 16:01:10 +0700

    first commit portofolio fauzan

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '20px',
+    },
+    extend: {
+      colors: {
+        primary: '#2dd4bf',
+        // primary: '#7E60BF',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
